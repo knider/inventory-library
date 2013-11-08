71a8bf1d3ef533e9c11462bee640b08f160af71c
Kevin Nider <knider@hotmail.com>
2013-11-07 21:37:36 -0800

    code cleanup and index fix

--- add_borrower.php
+++ add_borrower.php
@@ -1,19 +1,20 @@
 <?php
-ini_set('display_errors', 'On');
-$mysqli = new mysqli("oniddb.cws.oregonstate.edu","starkst-db","e0Wm80emmSOBOQSD","starkst-db");
-if($mysqli->connect_errno){
-		echo"Connection Error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
+	ini_set('display_errors', 'On');
+	$mysqli = new mysqli("oniddb.cws.oregonstate.edu","starkst-db","e0Wm80emmSOBOQSD","starkst-db");
+	if($mysqli->connect_errno){ echo"Connection Error " . $mysqli->connect_errno . " " . $mysqli->connect_error; }
+
+	$name = array_key_exists("name", $_POST) ? $_POST["name"] : '';
+	$email = array_key_exists("email", $_POST) ? $_POST["email"] : '';
+	$address = array_key_exists("address", $_POST) ? $_POST["address"] : '';
+	$phone = array_key_exists("phone", $_POST) ? $_POST["phone"] : '';
+
+	if ( !($stmt = $mysqli->prepare("INSERT INTO borrower(name,emailAddress,phoneNumber,streetAddress) values(?,?,?,?)") ) ) {
+		echo "Prepare failed: (" . $mysqli->errno . ")" . $mysqli->error;
 	}
-
-$name = $_POST["name"];
-$email = $_POST["email"];
-$address = $_POST["address"];
-$phone = $_POST["phone"];
-
-
-	if($stmt = $mysqli->prepare("insert into borrower(name,emailAddress,phoneNumber,streetAddress) values(?,?,?,?)")){
-		$stmt->bind_param('ssss', $name, $email, $phone, $address);
-		$stmt->execute();
+	if ( !$stmt->bind_param('ssss', $name, $email, $phone, $address) ) { 
+		echo "Bind paramaters failed: (" . $mysqli->errno . ")" . $mysqli->error; 
 	}
-header('location:post-login.php');
+	if ( !$stmt->execute() ) { echo "Execute failed: (" . $mysqli->errno . ")" . $mysqli->error; }
+	
+	header('location:tester.html');
 ?>
